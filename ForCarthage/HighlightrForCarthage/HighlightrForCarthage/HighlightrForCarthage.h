21af8e9d122b4d7ce9dc200c1718b56ebfa7e409
naotaka kanai <chokuryu@gmail.com>
2017-12-07 04:20:59 +0900

    Added xcode project for carthage. It requires manual build. It is using symlink to source.

---
+++ ForCarthage/HighlightrForCarthage/HighlightrForCarthage/HighlightrForCarthage.h
@@ -0,0 +1,19 @@
+//
+//  HighlightrForCarthage.h
+//  HighlightrForCarthage
+//
+//  Created by 金井直隆 on 2017/12/07.
+//  Copyright © 2017年 org_highlightr_name. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for HighlightrForCarthage.
+FOUNDATION_EXPORT double HighlightrForCarthageVersionNumber;
+
+//! Project version string for HighlightrForCarthage.
+FOUNDATION_EXPORT const unsigned char HighlightrForCarthageVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <HighlightrForCarthage/PublicHeader.h>
+
+
